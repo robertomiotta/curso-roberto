d79c086cca8e39045ce727b5fe7b7b6cfbaec1a4
robertomiotta <robertomiotta@gmail.com>
2021-09-27 22:13:48 -0300

    add meu primeiro projeto

---
+++ Aula01/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Aula01 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Curso.Roberto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Curso.Roberto
+{
+    public class  Program
+    {
+        public static void Main(string[] args) 
+        {
+            // meu primeiro projeto
+            var parm = Console.ReadLine();
+            int N = Convert.ToInt32(parm);
+            for (int i = 1; i <= N; i++) {
+                Console.WriteLine(i + " " + (i * i) + " " + (i * i * i));
+                Console.WriteLine(i + " " + ((i * i) + 1) + " " + ((i * i * i) + 1));
+            }
+
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine(i + " - Roberto");
+            }
+
+        }
+    }
+}
